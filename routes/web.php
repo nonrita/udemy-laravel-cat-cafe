f3a541a0dcdaaac5a05a04f57f49481ab4e147fa
nonrita <2220025@ecc.ac.jp>
2024-10-12 19:43:10 +0900

    フォーム入力内容の検証

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 Route::view('/', 'index');
+
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::post('/contact', [COntactController::class, 'sendMail'])->name('contact');
+Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
