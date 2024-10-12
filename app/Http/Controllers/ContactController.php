801caed7598fe634f641a47fa998bc50dcf667b5
nonrita <2220025@ecc.ac.jp>
2024-10-13 02:17:15 +0900

    定形メールの送信方法

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\ContactRequest;
+use App\Mail\ContactAdminMail;
 
 class ContactController extends Controller
 {
@@ -18,7 +20,7 @@ class ContactController extends Controller
 
         // これ以降の行は入力エラーがなかった場合のみ実行されます
         // 登録処理(実際はメール送信などを行う)
-        Log::debug($validated['name']. 'さんよりお問い合わせがありました');
+        Mail::to('admin@example.com')->send(new ContactAdminMail());
         return to_route('contact.complete');
     }
 
